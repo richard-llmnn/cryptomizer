e44442a73716819e380b3eacb0275e9a74f144e5
rullmann <69484873+richard-llmnn@users.noreply.github.com>
2022-06-17 17:06:06 +0200

    add de-/encrypt py path functions

--- src/crypto.py
+++ src/crypto.py
@@ -1,4 +1,6 @@
 import hashlib
+import os.path
+
 from Crypto.Cipher import AES
 from Crypto import Random
 from Crypto.Util.Padding import pad, unpad
@@ -6,8 +8,8 @@ from typing import Any
 
 DEFAULT_MODE = AES.MODE_GCM
 
-default_mode = AES.MODE_GCM
 
+ENCRYPTED_FILE_ENDING = ".crypt"
 
 def encrypt_file(binary_data: Any, key: str, block_size: int = 16) -> Any:
     # get 256 byte key
@@ -35,3 +37,31 @@ def decrypt_file(binary_data: Any, key: str, block_size: int = 16) -> Any:
         return unpad(encrypted_message, block_size)
     except Exception as exception:
         raise Exception("File could not be decrypted!")
+
+def encrypt_files_by_path(file_paths: list[str], output_folder: str, password: str) -> None:
+    for file_path in file_paths:
+        if not os.path.isabs(file_path):
+            raise Exception("File could not be opened!")
+
+        with open(file_path, "rb") as input_file:
+            filename = os.path.split(file_path)[1]
+
+            with open(os.path.join(output_folder, filename + ENCRYPTED_FILE_ENDING), "wb") as output_file:
+                output_file.write(encrypt_file(input_file.read(), password))
+
+
+def decrypt_files_by_path(file_paths: list[str], output_folder: str, password: str) -> None:
+    for file_path in file_paths:
+        if not os.path.isabs(file_path):
+            raise Exception("File could not be opened!")
+
+        with open(file_path, "rb") as input_file:
+            filename = os.path.split(file_path)[1]
+
+            if ENCRYPTED_FILE_ENDING not in filename:
+                raise Exception(f"The given file ${file_path} is not an encrypted file!")
+            # remove the ".crypt" from the file
+            filename = filename.removesuffix(ENCRYPTED_FILE_ENDING)
+
+            with open(os.path.join(output_folder, filename), "wb") as output_file:
+                output_file.write(decrypt_file(input_file.read(), password))
