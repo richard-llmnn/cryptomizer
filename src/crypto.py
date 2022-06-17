73fbab5e304063a2eb9cd9566c9d62447826d4f3
rullmann <69484873+richard-llmnn@users.noreply.github.com>
2022-06-17 17:05:24 +0200

    add type hinting and rename some variables

--- src/crypto.py
+++ src/crypto.py
@@ -2,14 +2,17 @@ import hashlib
 from Crypto.Cipher import AES
 from Crypto import Random
 from Crypto.Util.Padding import pad, unpad
+from typing import Any
+
+DEFAULT_MODE = AES.MODE_GCM
 
 default_mode = AES.MODE_GCM
 
 
-def encrypt_file(binary_data, key, block_size=16):
+def encrypt_file(binary_data: Any, key: str, block_size: int = 16) -> Any:
     # get 256 byte key
     password = hashlib.sha256(key.encode("utf-8")).digest()
-    mode = default_mode
+    mode = DEFAULT_MODE
     iv = Random.get_random_bytes(16)
     cypher = AES.new(password, mode, iv)
 
@@ -19,10 +22,10 @@ def encrypt_file(binary_data, key, block_size=16):
     return iv + encrypted_message
 
 
-def decrypt_file(binary_data, key, block_size=16):
+def decrypt_file(binary_data: Any, key: str, block_size: int = 16) -> Any:
     # get 256 byte key
     password = hashlib.sha256(key.encode("utf-8")).digest()
-    mode = default_mode
+    mode = DEFAULT_MODE
     # the first 16 bytes are the initialization vector
     iv = binary_data[0:16]
     cypher = AES.new(password, mode, iv)
